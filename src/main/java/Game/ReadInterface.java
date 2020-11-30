b239956e2369ae4e99e93b7b3eb6646b20e629bc
Bernabe Davila <juan.davila02@epn.edu.ec>
2020-11-30 09:30:08 -0500

    actualización de los miembros y comentario en la interfaz

--- src/main/java/Game/ReadInterface.java
+++ src/main/java/Game/ReadInterface.java
@@ -3,9 +3,10 @@ package Game;
 //Byron Huaraca
 //Jorge Alaba
 //Bernabé Dávila 
+//Karen Hurtado
 
 import javafx.collections.ObservableList;
-
+//Esta interface sirve para la utilizacion de algunas funciones fundamentales
 public interface ReadInterface {
 
     public ObservableList readTwo();
